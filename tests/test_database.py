351b2c066176a85542936ef55fab031c1ca130b7
Cameron Graybill <csg58@drexel.edu>
2018-05-21 20:15:47 -0400

    Finished initial database prototype with unit tests

---
+++ tests/test_database.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from server.db import DictionaryDB, Database
+
+
+class TestDictionaryDB(TestCase):
+    def test_normal_op(self):
+        db = DictionaryDB()
+        db.register_user(b"test", b"tset", 1234)
+        self.assertEqual(1234, db.get_rating(b"test"))
+        db.auth_user(b"test", b"tset")
+
+    def test_abnormal_op(self):
+        db = DictionaryDB()
+        db.register_user(b"a", b"a", 1)
+        with self.assertRaises(Database.DuplicateUser):
+            db.register_user(b"a", b"a", 1)
+        with self.assertRaises(Database.UserDoesNotExist):
+            db.auth_user(b"x", b"x")
+        with self.assertRaises(Database.UserDoesNotExist):
+            db.get_rating(b"x")
+        with self.assertRaises(Database.InvalidPassword):
+            db.auth_user(b"a", b"b")
